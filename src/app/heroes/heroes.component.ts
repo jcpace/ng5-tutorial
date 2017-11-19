756b8e45b503345e5f5c4811cfb48f4c8a08bad1
Joshua Pace <joshpace@gmail.com>
2017-11-19 11:54:33 -0800

    [add] service observable

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Hero } from '../hero'
 import { HEROES } from '../mock-heroes'
+import { HeroService } from '../hero.service'
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -9,20 +10,22 @@ import { HEROES } from '../mock-heroes'
 })
 export class HeroesComponent implements OnInit {
 
-  // hero: Hero = {
-  //   id: 1,
-  //   name: 'Windstorm'
-  // }
-  heroes = HEROES
+  heroes: Hero[];
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 
   selectedHero: Hero
   onSelect(hero: Hero): void {
     this.selectedHero = hero
   }
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes()
   }
 
 }
